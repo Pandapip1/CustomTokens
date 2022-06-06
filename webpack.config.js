d6524f1f19a49920104e8a5facfc166fa2dba9c8
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2022-06-06 15:47:48 +0000

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader')
+const webpack = require('webpack');
 
 module.exports = {
   entry: './index.js',
@@ -6,4 +8,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 };
